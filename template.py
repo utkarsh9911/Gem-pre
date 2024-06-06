67ddd2ec7f2db02a8326b0ed3dd77a7918a6a334
utkarsh9911 <utkarshraj7217@gmail.com>
2024-06-06 13:25:55 +0530

    logger and Exception compeleted

--- template.py
+++ template.py
@@ -16,7 +16,7 @@ list_of_files = [
     "src/pipeline/prediction_pipeline.py",
     "src/utils/__init__.py",
     "src/utils/utils.py",
-    "src/logger/logging.py",
+    "src/logging/logger.py",
     "src/exception/exception.py",
     "tests/unit/__init__.py",
     "tests/itegration/__init__.py",
