e0509a0f685268ab93b345e6935ec2e346cec1c9
utkarsh9911 <utkarshraj7217@gmail.com>
2024-06-11 19:22:34 +0530

    pipeline updated

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -35,7 +35,7 @@ def evaluate_model(X_train,y_train,X_test,y_test,models):
             # get R2 scores for train and test data
             test_model_score = r2_score(y_test,y_test_pred)
 
-            report[list(models.key())[i]] = test_model_score
+            report[list(models.keys())[i]] = test_model_score
         return report
     except Exception as e:
         logging.info("Exception occured during model training")
